f65068a1d9752aa45a5b1d90acec001e0129a84a
JohnnyC <johnnyfabiancardozo24@gmail.com>
2025-09-17 17:52:45 -0500

    Cambio desde comandos

--- VerficarVotoPersonaPython.py
+++ VerficarVotoPersonaPython.py
@@ -4,7 +4,7 @@
 #Entrada:Edad: Solicita la edad, Nacion: solicita su nacionalidad, nacion_en_minusculas: Convierte la variable nacion en minusculas
 #Salida:Si puede votar, No puede votar 
 #Proceso:Ingresa las varialbes, "edad" y "nacion", luego convierte la variable "nacion en minusculas", comprueba Sí "edad" es mayor o menor a 18, Sí su nacionalidad es; "colombiana o colombiano" Despliega un resultado, Sí alguno de los dos falla arroja otro.
-###
+#
 
 print("Verificar voto persona\n")
 
